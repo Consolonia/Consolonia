35271173b9cf051af24dead4c410fd74e0f124f4
Tom Laird-McConnell <thermous@iciclecreek.com>
2025-11-30 12:49:16 -0800

    define a cursor

--- src/Consolonia.Core/Infrastructure/ConsoleWindow.cs
+++ src/Consolonia.Core/Infrastructure/ConsoleWindow.cs
@@ -482,7 +482,7 @@ namespace Consolonia.Core.Infrastructure
         {
             return _cursorType switch
             {
-                StandardCursorType.Arrow => string.Empty,
+                StandardCursorType.Arrow => "◤",
                 StandardCursorType.Cross => "+",
                 StandardCursorType.Hand => "👆",
                 StandardCursorType.Help => "?",
