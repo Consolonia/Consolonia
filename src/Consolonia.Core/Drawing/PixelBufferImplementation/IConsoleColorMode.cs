8d4de5f86f56396a7e1c9f8fc390dc3084e1ee1d
Evgeny Gorbovoy <jinek@msn.com>
2026-01-03 22:19:05 +0100

    +todo

--- src/Consolonia.Core/Drawing/PixelBufferImplementation/IConsoleColorMode.cs
+++ src/Consolonia.Core/Drawing/PixelBufferImplementation/IConsoleColorMode.cs
@@ -15,6 +15,6 @@ namespace Consolonia.Core.Drawing.PixelBufferImplementation
         /// <summary>
         ///     Map standard avalonia colors to native platform colors supported by current color mode
         /// </summary>
-        (object background, object foreground) MapColors(Color background, Color foreground, FontWeight? weight);
+        (object background, object foreground) MapColors(Color background, Color foreground, FontWeight? weight);//todo: boxing of returned values!!
     }
 }
