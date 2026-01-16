46a1599fe56858e4fd74a5fca1da6792e8227544
Evgeny Gorbovoy <jinek@msn.com>
2026-01-16 05:38:15 +0100

    ComboBox bugs fixed

---
+++ src/Consolonia.Themes/Templates/Controls/Helpers/PopupPlacementToOuterMargin.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Globalization;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Data.Converters;
+
+namespace Consolonia.Themes.Templates.Controls.Helpers
+{
+    public sealed class PopupPlacementToOuterMargin : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (PlacementMode)value switch
+            {
+                PlacementMode.Top or PlacementMode.TopEdgeAlignedLeft or PlacementMode.TopEdgeAlignedRight => new Thickness(0, 0, 0, -1),
+                PlacementMode.Bottom or PlacementMode.BottomEdgeAlignedLeft or PlacementMode.BottomEdgeAlignedRight => new Thickness(0, -1, 0, 0),
+                PlacementMode.Left or PlacementMode.LeftEdgeAlignedBottom or PlacementMode.LeftEdgeAlignedTop => new Thickness(0, 0, -1, 0),
+                PlacementMode.Right or PlacementMode.RightEdgeAlignedBottom or PlacementMode.RightEdgeAlignedTop => new Thickness(-1, 0, 0, 0),
+                _ => throw new ArgumentOutOfRangeException(nameof(value), value, null)
+            };
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
