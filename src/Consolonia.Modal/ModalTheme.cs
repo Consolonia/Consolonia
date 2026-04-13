c2ee1b522cb5fb8320d6515025b71c9dfc8769d5
Evgeny Gorbovoy <jinek@msn.com>
2026-04-13 22:04:26 +0530

    Exerything looks good

--- src/Consolonia.Modal/ModalTheme.cs
+++ src/Consolonia.Modal/ModalTheme.cs
@@ -10,10 +10,12 @@ namespace Consolonia.Modal
             IncludeStyle(new Uri("avares://Consolonia.Modal/Themes/DialogHost.axaml"));
 
             switch (family)
-            {
+            { 
                 case TurboVisionThemeKey:
+                    IncludeStyle(new Uri("avares://Consolonia.Modal/Themes/TurboVision/DialogWindow.axaml"));
+                    break;
                 case ModernThemeKey:
-                    IncludeStyle(new Uri("avares://Consolonia.Modal/Themes/DialogWindow.axaml"));
+                    IncludeStyle(new Uri("avares://Consolonia.Modal/Themes/Modern/DialogWindow.axaml"));
                     break;
             }
         }
