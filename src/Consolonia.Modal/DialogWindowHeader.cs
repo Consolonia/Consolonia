8764838f61d1ec3a2e29e7c2b793c25d077dc7af
Evgeny Gorbovoy <jinek@msn.com>
2026-04-15 13:57:45 +0530

    dialog header added

---
+++ src/Consolonia.Modal/DialogWindowHeader.cs
@@ -0,0 +1,26 @@
+using Avalonia;
+using Avalonia.Controls.Primitives;
+
+namespace Consolonia.Modal
+{
+    public class DialogWindowHeader : TemplatedControl
+    {
+        public static readonly StyledProperty<string> TitleProperty =
+            DialogWindow.TitleProperty.AddOwner<DialogWindowHeader>();
+
+        public string Title
+        {
+            get => GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+
+        public static readonly StyledProperty<bool> IsCloseButtonVisibleProperty =
+            DialogWindow.IsCloseButtonVisibleProperty.AddOwner<DialogWindowHeader>();
+
+        public bool IsCloseButtonVisible
+        {
+            get => GetValue(IsCloseButtonVisibleProperty);
+            set => SetValue(IsCloseButtonVisibleProperty, value);
+        }
+    }
+}
