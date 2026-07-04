1b618e4de6087858fb70646f98547e4d7de867cb
Evgeny Gorbovoy <jinek@msn.com>
2026-07-05 00:44:17 +0100

    formatting, dictionary and stuff

--- src/Consolonia.Core/Text/Fonts/ConsolePlatformTypeface.cs
+++ src/Consolonia.Core/Text/Fonts/ConsolePlatformTypeface.cs
@@ -1,22 +1,14 @@
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Text;
 using Avalonia.Media;
 using Avalonia.Media.Fonts;
-using Consolonia.Core.Drawing;
 
 namespace Consolonia.Core.Text.Fonts
 {
-    internal sealed class ConsolePlatformTypeface : IPlatformTypeface
+    internal sealed class ConsolePlatformTypeface(IConsoleTypeface consoleTypeface) : IPlatformTypeface
     {
-        public ConsolePlatformTypeface(IConsoleTypeface consoleTypeface)
-        {
-            ConsoleTypeface = consoleTypeface;
-        }
-
-        public IConsoleTypeface ConsoleTypeface { get; }
+        public IConsoleTypeface ConsoleTypeface { get; } = consoleTypeface;
 
         public string FamilyName => ConsoleTypeface.FamilyName;
 
@@ -51,21 +43,21 @@ namespace Consolonia.Core.Text.Fonts
 
                     const int nameRecordsCount = 3;
                     const int stringOffset = 6 + nameRecordsCount * 12;
-                    var nameTable = new byte[stringOffset + familyNameBytes.Length + subfamilyNameBytes.Length +
-                                             fullNameBytes.Length];
+                    byte[] nameTable = new byte[stringOffset + familyNameBytes.Length + subfamilyNameBytes.Length +
+                                                fullNameBytes.Length];
 
                     // Header
                     nameTable[1] = 0x00; // format = 0
-                    nameTable[2] = (byte)(nameRecordsCount >> 8);
-                    nameTable[3] = (byte)nameRecordsCount;
-                    nameTable[4] = (byte)(stringOffset >> 8);
-                    nameTable[5] = (byte)stringOffset;
+                    nameTable[2] = 0;
+                    nameTable[3] = nameRecordsCount;
+                    nameTable[4] = 0;
+                    nameTable[5] = stringOffset;
 
                     int currentStringOffset = 0;
 
                     // NameRecord 1: Family Name
                     AddNameRecord(nameTable, 6, 1, familyNameBytes.Length, currentStringOffset);
-                    Array.Copy(familyNameBytes, 0, nameTable, stringOffset + currentStringOffset,
+                    Array.Copy(familyNameBytes, 0, nameTable, stringOffset,
                         familyNameBytes.Length);
                     currentStringOffset += familyNameBytes.Length;
 
@@ -123,22 +115,22 @@ namespace Consolonia.Core.Text.Fonts
 
         case /* hhea */ 1751672161u:
         {
-            var hhea = new byte[36];
+            byte[] hhea = new byte[36];
             hhea[1] = 0x01; // version = 1.0
 
-            var ascender = (short)-metrics.Ascent;
+            short ascender = (short)-metrics.Ascent;
             hhea[4] = (byte)(ascender >> 8);
             hhea[5] = (byte)ascender;
 
-            var descender = (short)-metrics.Descent;
+            short descender = (short)-metrics.Descent;
             hhea[6] = (byte)(descender >> 8);
             hhea[7] = (byte)descender;
 
-            var lineGap = (short)metrics.LineGap;
+            short lineGap = (short)metrics.LineGap;
             hhea[8] = (byte)(lineGap >> 8);
             hhea[9] = (byte)lineGap;
 
-            var advanceWidthMax = (ushort)metrics.DesignEmHeight;
+            ushort advanceWidthMax = metrics.DesignEmHeight;
             hhea[10] = (byte)(advanceWidthMax >> 8);
             hhea[11] = (byte)advanceWidthMax;
 
@@ -154,47 +146,47 @@ namespace Consolonia.Core.Text.Fonts
 
         case /* OS/2 */ 1330851634u:
         {
-            var os2 = new byte[86];
+            byte[] os2 = new byte[86];
 
             os2[1] = 0x01; // version = 1
 
-            var xAvgCharWidth = (short)metrics.DesignEmHeight;
+            short xAvgCharWidth = (short)metrics.DesignEmHeight;
             os2[2] = (byte)(xAvgCharWidth >> 8);
             os2[3] = (byte)xAvgCharWidth;
 
-            var weightClass = (ushort)ConsoleTypeface.Weight;
+            ushort weightClass = (ushort)ConsoleTypeface.Weight;
             os2[4] = (byte)(weightClass >> 8);
             os2[5] = (byte)weightClass;
 
             os2[7] = 0x05; // widthClass = 5
 
-            var strikethroughThickness = (short)metrics.StrikethroughThickness;
+            short strikethroughThickness = (short)metrics.StrikethroughThickness;
             os2[26] = (byte)(strikethroughThickness >> 8);
             os2[27] = (byte)strikethroughThickness;
 
-            var strikethroughPosition = (short)-metrics.StrikethroughPosition;
+            short strikethroughPosition = (short)-metrics.StrikethroughPosition;
             os2[28] = (byte)(strikethroughPosition >> 8);
             os2[29] = (byte)strikethroughPosition;
 
             os2[63] = 0xC0; // REGULAR | USE_TYPO_METRICS
 
-            var typoAscender = (short)-metrics.Ascent;
+            short typoAscender = (short)-metrics.Ascent;
             os2[68] = (byte)(typoAscender >> 8);
             os2[69] = (byte)typoAscender;
 
-            var typoDescender = (short)-metrics.Descent;
+            short typoDescender = (short)-metrics.Descent;
             os2[70] = (byte)(typoDescender >> 8);
             os2[71] = (byte)typoDescender;
 
-            var typoLineGap = (short)metrics.LineGap;
+            short typoLineGap = (short)metrics.LineGap;
             os2[72] = (byte)(typoLineGap >> 8);
             os2[73] = (byte)typoLineGap;
 
-            var winAscent = (ushort)-metrics.Ascent;
+            ushort winAscent = (ushort)-metrics.Ascent;
             os2[74] = (byte)(winAscent >> 8);
             os2[75] = (byte)winAscent;
 
-            var winDescent = (ushort)metrics.Descent;
+            ushort winDescent = (ushort)metrics.Descent;
             os2[76] = (byte)(winDescent >> 8);
             os2[77] = (byte)winDescent;
 
@@ -204,7 +196,7 @@ namespace Consolonia.Core.Text.Fonts
 
         case /* hmtx */ 1752003704u:
         {
-            var advanceWidth = (ushort)metrics.DesignEmHeight;
+            ushort advanceWidth = metrics.DesignEmHeight;
             table = new ReadOnlyMemory<byte>([
                 (byte)(advanceWidth >> 8), (byte)advanceWidth, // advanceWidth
                 0x00, 0x00 // leftSideBearing = 0
@@ -214,7 +206,7 @@ namespace Consolonia.Core.Text.Fonts
 
         case /* head */ 1751474532u:
         {
-            var head = new byte[54];
+            byte[] head = new byte[54];
 
             head[1] = 0x01; // version = 1.0
 
@@ -223,7 +215,7 @@ namespace Consolonia.Core.Text.Fonts
             head[14] = 0x3C;
             head[15] = 0xF5; // magicNumber
 
-            var unitsPerEm = (ushort)metrics.DesignEmHeight;
+            ushort unitsPerEm = metrics.DesignEmHeight;
             head[18] = (byte)(unitsPerEm >> 8);
             head[19] = (byte)unitsPerEm;
 
@@ -233,15 +225,15 @@ namespace Consolonia.Core.Text.Fonts
 
         case /* post */ 1886352244u:
         {
-            var post = new byte[32];
+            byte[] post = new byte[32];
 
             post[1] = 0x03; // version = 3.0
 
-            var underlinePosition = (short)-metrics.UnderlinePosition;
+            short underlinePosition = (short)-metrics.UnderlinePosition;
             post[8] = (byte)(underlinePosition >> 8);
             post[9] = (byte)underlinePosition;
 
-            var underlineThickness = (short)metrics.UnderlineThickness;
+            short underlineThickness = (short)metrics.UnderlineThickness;
             post[10] = (byte)(underlineThickness >> 8);
             post[11] = (byte)underlineThickness;
 
