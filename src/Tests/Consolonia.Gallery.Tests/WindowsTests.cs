a1d2f2f6663af34c0537c2ccf2dec8441b5c4aec
Evgeny Gorbovoy <jinek@msn.com>
2026-07-05 01:07:57 +0100

    Fixed Focus/Tab behavior of WindowsPanel. Brought back original Windows tests.

--- src/Tests/Consolonia.Gallery.Tests/WindowsTests.cs
+++ src/Tests/Consolonia.Gallery.Tests/WindowsTests.cs
@@ -1,11 +1,5 @@
 using System.Threading.Tasks;
-using System.Linq;
-using Avalonia;
-using Avalonia.Controls;
-using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Input;
-using Avalonia.Threading;
-using Avalonia.VisualTree;
 using Consolonia.Gallery.Tests.Base;
 using Consolonia.NUnit;
 using NUnit.Framework;
@@ -18,12 +12,17 @@ namespace Consolonia.Gallery.Tests
         [Test]
         public async Task PerformSingleTest()
         {
-            await FocusButton("Add Window");
+            await UITest.KeyInput(Key.Tab);
+            // await UITest.KeyInput(Key.Down); // change startup to be centerowner
+            await UITest.KeyInput(Key.Tab);
+            await UITest.KeyInput(Key.Down); // change startup to be maximized
+            await UITest.KeyInput(Key.Down); // change startup to be maximized
+            await UITest.KeyInput(Key.Tab);
             await UITest.KeyInput(Key.Enter); // add window
             await Task.Delay(100); // animation
             await UITest.AssertHasText("New Window 1");
-            await UITest.AssertHasText("🗕", "🗖", "🗙");
-            await UITest.AssertHasNoText("🗗");
+            await UITest.AssertHasText("🗕", "🗗", "🗙");
+            await UITest.AssertHasNoText("🗖");
             await UITest.KeyInput(Key.Tab);
             await UITest.KeyInput(Key.Tab);
             await UITest.KeyInput(Key.Tab);
@@ -31,24 +30,8 @@ namespace Consolonia.Gallery.Tests
             await UITest.KeyInput(Key.Tab);
             await UITest.KeyInput(Key.Enter); // close button
             await Task.Delay(100); // animation
-            foreach (string x in new[] { "New Window 1", "DialogResult:", "🗕", "🗖", "🗙" })
+            foreach (string x in new[] { "New Window 1", "DialogResult:", "🗕", "🗗", "🗙" })
                 await UITest.AssertHasNoText(x);
-        }
-
-        private static async Task FocusButton(string content)
-        {
-            await Dispatcher.UIThread.InvokeAsync(() =>
-            {
-                var mainWindow =
-                    (Window)
-                    ((IClassicDesktopStyleApplicationLifetime)Application.Current!.ApplicationLifetime)!.MainWindow!;
-                Button button = mainWindow.GetVisualDescendants()
-                    .OfType<Button>()
-                    .First(x => x.Content?.ToString() == content);
-                button.Focus(NavigationMethod.Tab);
-            });
-
-            await UITest.WaitRendered();
         }
     }
 }
